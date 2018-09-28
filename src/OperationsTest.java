2d1b458cbd00569ae761aca634f021a5efcc868f
Jonathanma21 <42975675+Jonathanma21@users.noreply.github.com>
2018-09-28 12:27:34 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,35 @@
+
+public class OperationsTest 
+{
+
+	public static void main(String[] args) 
+	{
+		 int value; // Declaration Statement
+
+	     	value = 5; //Assignment statement -> initialization
+	     	
+	     	System.out.println("Original Variable value: " + value);
+
+	        value += 5; // value = value + 5 
+
+	        System.out.println("Add 5: " + value); 
+
+	        value /= 2; 
+
+	        System.out.println("Divide 2: " + value); 
+
+	        value *= 4; 
+
+	        System.out.println("Multiply by 4: " + value); 
+
+	        value -= 6; 
+
+	        System.out.println("Subtract by 6: " + value); 
+
+	        value %= 2; 
+
+	        System.out.println("Modular divide by 2: " + value); 
+
+	}
+
+}
